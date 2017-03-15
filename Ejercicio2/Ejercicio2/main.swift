cc1c628835cc2d8f70c21c555e4735ef138bd343
Daniel Zanabria González <daniel@WizCode-MBP-2021.local>
2017-03-15 09:56:56 -0600

    Prueba de repositorio

---
+++ Ejercicio2/Ejercicio2/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Ejercicio2
+//
+//  Created by Daniel Zanabria González on 15/03/17.
+//  Copyright © 2017 Daniel Zanabria González. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+print("daniel Zanabria")
+
+
