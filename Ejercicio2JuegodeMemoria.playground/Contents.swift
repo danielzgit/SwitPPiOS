4ce55690c7dafa0962a869b23567748c48fd4a3a
Daniel Zanabria González <daniel@WizCode-MBP-2021.local>
2017-03-15 22:28:14 -0600

    Tarea 2 del curso Swift Programar para iOS resuelta

--- Ejercicio2JuegodeMemoria.playground/Contents.swift
+++ Ejercicio2JuegodeMemoria.playground/Contents.swift
@@ -2,8 +2,30 @@
 
 import UIKit
 
-var str = "Hello, playground"
+/*
+ Reto número 2 curso Swift Programar para iOS
+ Estudiante: Daniel Zanabria González
+ usuario: danielzglez@yahoo.com.mx
+ */
 
-var prueba = "Esta es una prueba"
-
-var tarea = "Daniel Zanabria"
+//imprimo los números del 0 al 100
+for i in 0...100{
+  //reviso si el número es par
+  if(i%2==0){
+    //el numero es par
+    print("\(i) es par")
+  }else{
+    //el numero es impar
+    print("\(i) es impar")
+  }
+  //reviso si el número es divisible entre 5
+  if(i%5==0){
+    //es divisible entre 5
+    print("\(i) Bingo!!!")
+  }
+  //reviso si se encuentra entre 30 y 40
+  if(i>=30 && i<=40){
+    //se encuentra en el rango 30-40
+    print("\(i) Viva Swift!!!")
+  }
+}
